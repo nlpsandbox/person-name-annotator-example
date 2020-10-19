a58b7eba436b7975b82a3112aa54d874be44c667
tschaffter <thomas.schaffter@gmail.com>
2020-10-18 17:58:25 -0700

    Detect last names based on census.gov top 1000 lastnames

--- server/openapi_server/controllers/person_name_controller.py
+++ server/openapi_server/controllers/person_name_controller.py
@@ -6,6 +6,9 @@ import spacy
 import json
 import datetime
 from pprint import pprint
+import csv
+import os.path
+from os import path
 
 from openapi_server.models.note import Note
 from openapi_server.models.person_name_annotation import \
@@ -18,40 +21,86 @@ def person_names_read_all(note=None):  # noqa: E501
 
     Returns the person name annotations # noqa: E501
 
-    :param note: 
+    :param note:
     :type note: list | bytes
 
     :rtype: List[PersonNameAnnotation]
     """
-    nlp = Config.nlp
-    logging.info(f"NOTE TEXT: started")
-    found = False
-    counter = 1
-    return_list = []
-    if connexion.request.is_json:
-        found = True
-        note = [Note.from_dict(d) for d in
-                connexion.request.get_json()]  # noqa: E501
-        note_text = note[0]._text
-        note_id = note[0]._id
-        logging.info(f"NOTE TEXT: {note_text}")
-        nlp = spacy.load('en_core_web_md')
-        doc = nlp(note_text)
-        for sent in doc.sents:
-            for entity in sent.ents:
-                logging.info(
-                    f"ENTITY found {entity} with label {entity.label_}")
-                if entity.label_ == 'PERSON':
-                    logging.info(f"{entity.text}  {entity.start_char} ")
-                    return_list = add_match(counter, entity, note, return_list,
-                                            note_id)
-                    counter += 1
-                    # logging.info(f"PRETTY : { pprint(return_list)}")
+    res = []
 
-        for m in return_list:
-            logging.info(f" text set to : {m.text}")
+    # with open('') as csv_file:
+    #     csv_reader = csv.reader(csv_file, delimiter=',')
+    #     line_count = 0
+    #     for row in csv_reader:
+    #         if line_count == 0:
+    #             print(f'Column names are {", ".join(row)}')
+    #             line_count += 1
+    #         else:
+    #             print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+    #             line_count += 1
+    #     print(f'Processed {line_count} lines.')
 
-    return return_list
+
+    # if connexion.request.is_json:
+    #     notes = [Note.from_dict(d) for d in connexion.request.get_json()]  # noqa: E501
+
+    #     for note in notes:
+    print("plop")
+    if path.exists("data/census_gov_top_1000_lastnames.csv"):
+        logging.info("CSV EXISTS")
+    else:
+        logging.info("CSV DOES NOT EXIST")
+
+
+            # # Adapted from https://stackoverflow.com/a/61234139
+            # matches = re.finditer('([1-9]|0[1-9]|1[0-9]|2[0-9]|3[0-1])(/)([1-9]|0[1-9]|1[0-2])(/)(19[0-9][0-9]|20[0-9][0-9])', note._text)
+            # add_date_annotation(res, note, matches, "MM/DD/YYYY")
+
+            # matches = re.finditer('([1-9]|0[1-9]|1[0-9]|2[0-9]|3[0-1])(-)([1-9]|0[1-9]|1[0-2])(-)(19[0-9][0-9]|20[0-9][0-9])', note._text)
+            # add_date_annotation(res, note, matches, "MM-DD-YYYY")
+
+            # matches = re.finditer('([1-9]|0[1-9]|1[0-9]|2[0-9]|3[0-1])(\.)([1-9]|0[1-9]|1[0-2])(\.)(19[0-9][0-9]|20[0-9][0-9])', note._text)
+            # add_date_annotation(res, note, matches, "MM.DD.YYYY")
+
+            # matches = re.finditer('([1-9][1-9][0-9][0-9]|2[0-9][0-9][0-9])', note._text)
+            # add_date_annotation(res, note, matches, "YYYY")
+
+            # matches = re.finditer('(January|February|March|April|May|June|July|August|September|October|November|December)', note._text, re.IGNORECASE)
+            # add_date_annotation(res, note, matches, "MMMM")
+
+            # # TODO: Remove annotations that are fully included into another
+            # # annotation.
+
+    return jsonify(res)
+    # nlp = Config.nlp
+    # logging.info(f"NOTE TEXT: started")
+    # found = False
+    # counter = 1
+    # return_list = []
+    # if connexion.request.is_json:
+    #     found = True
+    #     note = [Note.from_dict(d) for d in
+    #             connexion.request.get_json()]  # noqa: E501
+    #     note_text = note[0]._text
+    #     note_id = note[0]._id
+    #     logging.info(f"NOTE TEXT: {note_text}")
+    #     nlp = spacy.load('en_core_web_md')
+    #     doc = nlp(note_text)
+    #     for sent in doc.sents:
+    #         for entity in sent.ents:
+    #             logging.info(
+    #                 f"ENTITY found {entity} with label {entity.label_}")
+    #             if entity.label_ == 'PERSON':
+    #                 logging.info(f"{entity.text}  {entity.start_char} ")
+    #                 return_list = add_match(counter, entity, note, return_list,
+    #                                         note_id)
+    #                 counter += 1
+    #                 # logging.info(f"PRETTY : { pprint(return_list)}")
+
+    #     for m in return_list:
+    #         logging.info(f" text set to : {m.text}")
+
+    # return return_list
 
 
 def add_match(counter, entity, note, returnList, note_id):
--- server2/openapi_server/controllers/person_name_controller.py
+++ server2/openapi_server/controllers/person_name_controller.py
@@ -1,5 +1,9 @@
 import connexion
 import six
+import csv
+import pandas as pd
+import re
+from flask import jsonify
 
 from openapi_server.models.note import Note  # noqa: E501
 from openapi_server.models.person_name_annotation import PersonNameAnnotation  # noqa: E501
@@ -11,11 +15,32 @@ def person_names_read_all(note=None):  # noqa: E501
 
     Returns the person name annotations # noqa: E501
 
-    :param note: 
+    :param note:
     :type note: list | bytes
 
     :rtype: List[PersonNameAnnotation]
     """
+    # Get dictionary of top 1000 last names from census.gov (18-10-2020)
+    # Source: https://www.census.gov/topics/population/genealogy/data/2000_surnames.html
+    # TODO: Cache the data
+    df = pd.read_csv("data/census_gov_top_1000_lastnames.csv")
+    lastnames = df['name'].str.lower().unique().tolist()
+
+    # TODO: Get data from https://cloud.google.com/bigquery/public-data/
+
+    res = []
     if connexion.request.is_json:
-        note = [Note.from_dict(d) for d in connexion.request.get_json()]  # noqa: E501
-    return 'do some magic!'
+        notes = [Note.from_dict(d) for d in connexion.request.get_json()]  # noqa: E501
+
+        for note in notes:
+            for name in lastnames:
+                matches = re.finditer(name, note._text, re.IGNORECASE)
+                for match in matches:
+                    res.append({
+                        'noteId': note._id,
+                        'text':  match[0],
+                        'start': match.start(),
+                        'length': len(match[0])
+                    })
+
+    return jsonify(res)
