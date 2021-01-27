c2dce0851f0c8bed19cc05a0b288c74653a59b27
tschaffter <thomas.schaffter@gmail.com>
2021-01-27 10:05:11 -0800

    Fix flake8 and tox

---
+++ server/openapi_server/controllers/tool_controller.py
@@ -0,0 +1,37 @@
+from openapi_server.models.tool import Tool  # noqa: E501
+# from openapi_server.models.tool_dependencies import ToolDependencies  # noqa: E501
+
+
+def get_tool():  # noqa: E501
+    """Get tool information
+
+    Get information about the tool # noqa: E501
+
+
+    :rtype: Tool
+    """
+    tool = Tool(
+        name="person-name-annotator-example",
+        version="0.3.1",
+        license="apache-2.0",
+        repository="github:nlpsandbox/person-name-annotator-example",
+        description="An example implementation of the NLP Sandbox Person " +
+                    "Name Annotator",
+        author="The NLP Sandbox Team",
+        author_email="thomas.schaffter@sagebionetworks.org",
+        url="https://github.com/nlpsandbox/person-name-annotator-example",
+        tool_type="nlpsandbox:person-name-annotator"
+    )
+
+    return tool, 200
+
+
+def get_tool_dependencies():  # noqa: E501
+    """Get tool dependencies
+
+    Get the dependencies of this tool # noqa: E501
+
+
+    :rtype: ToolDependencies
+    """
+    return [], 200
