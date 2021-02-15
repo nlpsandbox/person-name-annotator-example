694fc5b44334b507429f613742e332b6ea34026c
tschaffter <thomas.schaffter@gmail.com>
2021-02-14 19:30:38 -0800

    Re-generate codebase using schemas 1.0.0

--- server/openapi_server/controllers/text_person_name_annotation_controller.py
+++ server/openapi_server/controllers/text_person_name_annotation_controller.py
@@ -5,7 +5,7 @@ import re
 from openapi_server.models.error import Error  # noqa: E501
 from openapi_server.models.text_person_name_annotation_request import TextPersonNameAnnotationRequest  # noqa: E501
 from openapi_server.models.text_person_name_annotation import TextPersonNameAnnotation  # noqa: E501
-from openapi_server.models.text_person_name_annotations import TextPersonNameAnnotations  # noqa: E501
+from openapi_server.models.text_person_name_annotation_response import TextPersonNameAnnotationResponse  # noqa: E501
 
 
 class Data:
@@ -29,7 +29,7 @@ def create_text_person_name_annotations():  # noqa: E501
 
     Return the person name annotations found in a clinical note # noqa: E501
 
-    :rtype: TextPersonNameAnnotations
+    :rtype: TextPersonNameAnnotationResponse
     """
     res = None
     status = None
@@ -50,7 +50,7 @@ def create_text_person_name_annotations():  # noqa: E501
                             text=match[0],
                             confidence=95.5
                         ))
-            res = TextPersonNameAnnotations(annotations)
+            res = TextPersonNameAnnotationResponse(annotations)
             status = 200
         except Exception as error:
             status = 500
--- server/openapi_server/models/__init__.py
+++ server/openapi_server/models/__init__.py
@@ -10,6 +10,6 @@ from openapi_server.models.note import Note
 from openapi_server.models.text_annotation import TextAnnotation
 from openapi_server.models.text_person_name_annotation import TextPersonNameAnnotation
 from openapi_server.models.text_person_name_annotation_request import TextPersonNameAnnotationRequest
-from openapi_server.models.text_person_name_annotations import TextPersonNameAnnotations
+from openapi_server.models.text_person_name_annotation_response import TextPersonNameAnnotationResponse
 from openapi_server.models.tool import Tool
 from openapi_server.models.tool_dependencies import ToolDependencies
